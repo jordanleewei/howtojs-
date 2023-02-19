2979a1806b77a4e8af253ff0c0ad3ad56d857f33
jordanleewei <94658443+jordanleewei@users.noreply.github.com>
2023-02-20 00:49:38 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,11 @@ export default function Home() {
             <iframe width="100%" height="100%" src="https://www.youtube.com/embed/uYuOaES7PDA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
           </div>
         </div>
+        <div class="react-player-wrapper">
+          <div class="react-player">
+            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/uYuOaES7PDA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+        </div>
       </main>
 
       <Footer />
