5eb0e27a0445faf6a65a188c9d1f8b64b1763f2e
jordanleewei <94658443+jordanleewei@users.noreply.github.com>
2023-02-19 23:33:57 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import YouTube from 'react-youtube';
 
 export default function Home() {
+  const videoId = 'uYuOaES7PDA'; // replace with your YouTube video ID
+
   return (
     <div className="container">
       <Head>
@@ -13,11 +16,12 @@ export default function Home() {
       <main>
         <Header title="Welcome! You have found my secret website!" />
         <p className="description">
-           Take a look at some of my projects.
+          Take a look at some of my projects.
         </p>
+        <YouTube videoId={videoId} />
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
