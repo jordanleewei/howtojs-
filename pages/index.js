1f7a8fef0542f4a3b9b1f3180917431e29c46fb2
jordanleewei <94658443+jordanleewei@users.noreply.github.com>
2023-02-20 03:33:34 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/globals.css';
 import ReactPlayer from 'react-player';
 
 export default function Home() {
