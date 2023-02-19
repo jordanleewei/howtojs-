ed60a51595696e428eebfde9e74117bd42c67dda
jordanleewei <94658443+jordanleewei@users.noreply.github.com>
2023-02-20 03:12:20 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,15 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
-import YouTube from 'react-youtube'
-import { useState } from 'react'
+import { useState } from 'react';
+import Head from 'next/head';
+import Header from '@components/Header';
+import Footer from '@components/Footer';
+import ReactPlayer from 'react-player';
 
 export default function Home() {
-  const opts = {
-    height: '390',
-    width: '640',
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 0,
-    },
-  }
-  
-  const [showVideos, setShowVideos] = useState(false);
-  const [currentVideo, setCurrentVideo] = useState(0);
+  const [showVideo, setShowVideo] = useState(false);
 
-  const handleShowVideos = () => {
-    setShowVideos(true);
-  }
-
-  const handleNextVideo = () => {
-    setCurrentVideo(currentVideo + 1);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  }
+  const handleShowVideo = () => {
+    setShowVideo(true);
+  };
 
   return (
     <div className="container">
@@ -35,33 +20,85 @@ export default function Home() {
 
       <main>
         <Header title="Welcome! You have found my secret website!" />
-        <p className="description">
-           Take a look at some of my projects.
-        </p>
-        {!showVideos &&
-          <button onClick={handleShowVideos}>Time to explore!</button>
-        }
-        {showVideos &&
-          <div>
-            <div className="video-container">
-              {currentVideo === 0 &&
-                <YouTube videoId="uYuOaES7PDA" opts={opts} onReady={(event) => event.target.playVideo()} />
-              }
-              {currentVideo === 1 &&
-                <YouTube videoId="PxjKNODUk1A" opts={opts} onReady={(event) => event.target.playVideo()} />
-              }
-              {currentVideo === 2 &&
-                <YouTube videoId="lGnazT38vWc" opts={opts} onReady={(event) => event.target.playVideo()} />
-              }
-            </div>
-            <div className="video-controls">
-              <button onClick={handleNextVideo} disabled={currentVideo === 2}>Next Video</button>
+        <p className="description">Take a look at some of my projects.</p>
+
+        <button className="explore-button" onClick={handleShowVideo}>
+          Time to explore!
+        </button>
+
+        {showVideo && (
+          <div className="video-container">
+            <ReactPlayer
+              url="https://www.youtube.com/watch?v=uYuOaES7PDA"
+              width="100%"
+              height="100%"
+            />
+            <div className="scroll-button" onClick={() => window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })}>
+              Scroll to next video
             </div>
           </div>
-        }
+        )}
       </main>
 
       <Footer />
+
+      <style jsx>{`
+        .explore-button {
+          background-color: #f53b57;
+          border: none;
+          border-radius: 8px;
+          color: #fff;
+          cursor: pointer;
+          font-size: 24px;
+          padding: 16px;
+          transition: opacity 0.5s;
+        }
+
+        .explore-button:hover {
+          opacity: 0.8;
+        }
+
+        .video-container {
+          opacity: 0;
+          transition: opacity 1s;
+        }
+
+        .video-container.show {
+          opacity: 1;
+        }
+
+        .scroll-button {
+          background-color: #f53b57;
+          border: none;
+          border-radius: 8px;
+          color: #fff;
+          cursor: pointer;
+          font-size: 18px;
+          padding: 12px 24px;
+          position: absolute;
+          right: 32px;
+          top: 50%;
+          transform: translateY(-50%);
+          transition: opacity 0.5s;
+        }
+
+        .scroll-button:hover {
+          opacity: 0.8;
+        }
+      `}</style>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+            Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
     </div>
-  )
+  );
 }
